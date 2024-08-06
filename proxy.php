386cd61df109e0b4c01619262fb39e6466f10758
otrashpost <nerddpetter155@gmail.com>
2024-08-06 16:04:56 -0300

    Update and rename proxy.php to proxy.mpd

--- proxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = 'https://0108-vos.dtvott.com/DASH/manifest.mpd'; // Substitua pelo caminho real do seu arquivo .m3u8
-header('Content-Type: application/vnd.apple.mpegurl');
-readfile($url);
-?>
